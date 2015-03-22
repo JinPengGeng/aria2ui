38354e5eb4c771559462413f272046d4b5dcfc05
Zachary Boyd <zacharyboyd@znetstar.net>
2015-03-22 05:38:19 -0400

    Allows regular aria2 command line switches to be passed

--- bin/aria2ui.js
+++ bin/aria2ui.js
@@ -8,4 +8,5 @@ var path = require('path'),
 var base_path = path.join(path.dirname(fs.realpathSync(__filename)), '..');
 var nw = path.join(base_path, 'node_modules', '.bin', 'nw');
 
-exec(nw+' '+base_path, { async: false, silent: false }, function (code, output) {});
+var command = nw+' '+base_path+' '+process.argv.slice(2).join(' ');
+exec(command, { async: false, silent: false }, function (code, output) {});
