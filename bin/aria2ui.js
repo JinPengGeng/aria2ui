025f6e49d325fbd81b503eca89c70e371525ac41
Zachary Boyd <zacharyboyd@znetstar.net>
2015-03-22 04:22:12 -0400

    adds executable

--- bin/aria2ui.js
+++ bin/aria2ui.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
+var path = require('path'),
+	fs = require('fs');
+
 (require('shelljs/global'))
 
-var base_path = path.join(path.dirname(fs.realpathSync(__filename));
+var base_path = path.join(path.dirname(fs.realpathSync(__filename)), '..');
 var nw = path.join(base_path, 'node_modules', '.bin', 'nw');
 
-exec(nw+' '+base_path, { async: false, silent: false }, function () {});
+exec(nw+' '+base_path, { async: false, silent: false }, function (code, output) {});
